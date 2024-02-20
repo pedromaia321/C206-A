859d55f1b7a1de064fd68e1112d5ed88e3e696fc
Pedro Cesar Maia <111019506+pedromaia321@users.noreply.github.com>
2024-02-20 14:40:51 -0300

    Add files via upload

---
+++ Exercicio_do_Ze_do_Lanche/src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int lanche1 = 10;
+        int lanche2 = 4;
+        int lanche3 = 2;
+
+        int total = lanche1+lanche2+lanche3;
+        float media = total/3.0f;
+
+
+
+        System.out.println("O total de lanches foi "+total);
+        System.out.println("A media de lanches foi " +media);
+
+    }
+}
